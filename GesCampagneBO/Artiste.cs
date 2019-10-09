3adeab3849d977871cb166c005482598358b1725
Jasonoooo <45065804+Jasonoooo@users.noreply.github.com>
2019-10-09 17:09:06 +0200

    Modification des noms
    Modifications des noms de classe "class1"

--- GesCampagneBO/Artiste.cs
+++ GesCampagneBO/Artiste.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace GesCampagneBO
 {
-    public class Class1
+    public class Artiste
     {
     }
 }
--- GesCampagneDAL/ArtisteDAO.cs
+++ GesCampagneDAL/ArtisteDAO.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace GesCampagneDAL
 {
-    public class Class1
+    public class ArtisteDAO
     {
     }
 }
