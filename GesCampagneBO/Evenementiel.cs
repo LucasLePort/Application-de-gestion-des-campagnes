3d13c290b470491fc32c30ed280912e45acac31f
Cartagene <45066097+Cartagene@users.noreply.github.com>
2019-10-10 10:25:10 +0200

    validé

--- GesCampagneBO/Evenementiel.cs
+++ GesCampagneBO/Evenementiel.cs
@@ -6,13 +6,33 @@ using System.Threading.Tasks;
 
 namespace GesCampagneBO
 {
-    public class Evenementiel
+    class Evenementiel
     {
+        private int id;
         private string nom;
         private string rue;
         private string telephone;
         private string mail;
         private string site;
-        private Ville laVille; 
+        private Ville laVille;
+
+        public Evenementiel(int id, string nom, string rue, string telephone, string mail, string site, Ville laVille)
+        {
+            this.id = id;
+            this.nom = nom;
+            this.rue = rue;
+            this.telephone = telephone;
+            this.mail = mail;
+            this.site = site;
+            this.laVille = laVille;
+        }
+
+        public int Id { get => id; set => id = value; }
+        public string Nom { get => nom; set => nom = value; }
+        public string Rue { get => rue; set => rue = value; }
+        public string Telephone { get => telephone; set => telephone = value; }
+        public string Mail { get => mail; set => mail = value; }
+        public string Site { get => site; set => site = value; }
+        public Ville LaVille { get => laVille; set => laVille = value; }
     }
 }
