6a980ef669e35b3d4e1465e968cadfefbbda37d0
Cartagene <45066097+Cartagene@users.noreply.github.com>
2019-10-10 09:39:43 +0200

    creationn formulaire ajout agence

--- GesCampagneGUI/FrmAjoutAgence.Designer.cs
+++ GesCampagneGUI/FrmAjoutAgence.Designer.cs
@@ -31,18 +31,25 @@
             this.lblNom = new System.Windows.Forms.Label();
             this.lblVille = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
-            this.grbAdresse = new System.Windows.Forms.GroupBox();
+            this.lblRue = new System.Windows.Forms.Label();
             this.lblMail = new System.Windows.Forms.Label();
             this.lblTel = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
-            this.lblRue = new System.Windows.Forms.Label();
-            this.grbAdresse.SuspendLayout();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.rbtComm = new System.Windows.Forms.RadioButton();
+            this.rbtEvent = new System.Windows.Forms.RadioButton();
+            this.lblTypeAgence = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // lblNom
             // 
             this.lblNom.AutoSize = true;
-            this.lblNom.Location = new System.Drawing.Point(195, 68);
+            this.lblNom.Location = new System.Drawing.Point(217, 36);
             this.lblNom.Name = "lblNom";
             this.lblNom.Size = new System.Drawing.Size(93, 13);
             this.lblNom.TabIndex = 0;
@@ -52,7 +59,7 @@
             // lblVille
             // 
             this.lblVille.AutoSize = true;
-            this.lblVille.Location = new System.Drawing.Point(6, 63);
+            this.lblVille.Location = new System.Drawing.Point(277, 286);
             this.lblVille.Name = "lblVille";
             this.lblVille.Size = new System.Drawing.Size(32, 13);
             this.lblVille.TabIndex = 3;
@@ -61,26 +68,24 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(195, 191);
+            this.label5.Location = new System.Drawing.Point(195, 159);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(0, 13);
             this.label5.TabIndex = 4;
             // 
-            // grbAdresse
+            // lblRue
             // 
-            this.grbAdresse.Controls.Add(this.lblRue);
-            this.grbAdresse.Controls.Add(this.lblVille);
-            this.grbAdresse.Location = new System.Drawing.Point(198, 179);
-            this.grbAdresse.Name = "grbAdresse";
-            this.grbAdresse.Size = new System.Drawing.Size(342, 149);
-            this.grbAdresse.TabIndex = 5;
-            this.grbAdresse.TabStop = false;
-            this.grbAdresse.Text = "Adresse";
+            this.lblRue.AutoSize = true;
+            this.lblRue.Location = new System.Drawing.Point(277, 245);
+            this.lblRue.Name = "lblRue";
+            this.lblRue.Size = new System.Drawing.Size(33, 13);
+            this.lblRue.TabIndex = 4;
+            this.lblRue.Text = "Rue :";
             // 
             // lblMail
             // 
             this.lblMail.AutoSize = true;
-            this.lblMail.Location = new System.Drawing.Point(195, 125);
+            this.lblMail.Location = new System.Drawing.Point(238, 109);
             this.lblMail.Name = "lblMail";
             this.lblMail.Size = new System.Drawing.Size(72, 13);
             this.lblMail.TabIndex = 6;
@@ -89,45 +94,118 @@
             // lblTel
             // 
             this.lblTel.AutoSize = true;
-            this.lblTel.Location = new System.Drawing.Point(195, 96);
+            this.lblTel.Location = new System.Drawing.Point(195, 66);
             this.lblTel.Name = "lblTel";
-            this.lblTel.Size = new System.Drawing.Size(109, 13);
+            this.lblTel.Size = new System.Drawing.Size(115, 13);
             this.lblTel.TabIndex = 7;
-            this.lblTel.Text = "Numéro de téléphone";
+            this.lblTel.Text = "Numéro de téléphone :";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(195, 150);
+            this.label1.Location = new System.Drawing.Point(241, 146);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(69, 13);
             this.label1.TabIndex = 8;
             this.label1.Text = "Site internet :";
             // 
-            // lblRue
+            // textBox1
             // 
-            this.lblRue.AutoSize = true;
-            this.lblRue.Location = new System.Drawing.Point(6, 31);
-            this.lblRue.Name = "lblRue";
-            this.lblRue.Size = new System.Drawing.Size(27, 13);
-            this.lblRue.TabIndex = 4;
-            this.lblRue.Text = "Rue";
+            this.textBox1.Location = new System.Drawing.Point(316, 33);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 9;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(316, 63);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 10;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(316, 102);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 11;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(316, 139);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 12;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(316, 242);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 5;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(316, 283);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 6;
+            // 
+            // rbtComm
+            // 
+            this.rbtComm.AutoSize = true;
+            this.rbtComm.Location = new System.Drawing.Point(391, 204);
+            this.rbtComm.Name = "rbtComm";
+            this.rbtComm.Size = new System.Drawing.Size(97, 17);
+            this.rbtComm.TabIndex = 13;
+            this.rbtComm.TabStop = true;
+            this.rbtComm.Text = "Communication";
+            this.rbtComm.UseVisualStyleBackColor = true;
+            // 
+            // rbtEvent
+            // 
+            this.rbtEvent.AutoSize = true;
+            this.rbtEvent.Location = new System.Drawing.Point(296, 204);
+            this.rbtEvent.Name = "rbtEvent";
+            this.rbtEvent.Size = new System.Drawing.Size(89, 17);
+            this.rbtEvent.TabIndex = 14;
+            this.rbtEvent.TabStop = true;
+            this.rbtEvent.Text = "Evenementiel";
+            this.rbtEvent.UseVisualStyleBackColor = true;
+            // 
+            // lblTypeAgence
+            // 
+            this.lblTypeAgence.AutoSize = true;
+            this.lblTypeAgence.Location = new System.Drawing.Point(195, 204);
+            this.lblTypeAgence.Name = "lblTypeAgence";
+            this.lblTypeAgence.Size = new System.Drawing.Size(95, 13);
+            this.lblTypeAgence.TabIndex = 15;
+            this.lblTypeAgence.Text = "Type de l\'agence :";
             // 
             // FrmAjoutAgence
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblTypeAgence);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.rbtEvent);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.rbtComm);
+            this.Controls.Add(this.lblRue);
+            this.Controls.Add(this.lblVille);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.lblTel);
             this.Controls.Add(this.lblMail);
-            this.Controls.Add(this.grbAdresse);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.lblNom);
             this.Name = "FrmAjoutAgence";
             this.Text = "Ajout d\'une agence";
-            this.grbAdresse.ResumeLayout(false);
-            this.grbAdresse.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -138,10 +216,18 @@
         private System.Windows.Forms.Label lblNom;
         private System.Windows.Forms.Label lblVille;
         private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.GroupBox grbAdresse;
         private System.Windows.Forms.Label lblMail;
         private System.Windows.Forms.Label lblTel;
         private System.Windows.Forms.Label lblRue;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.RadioButton rbtComm;
+        private System.Windows.Forms.RadioButton rbtEvent;
+        private System.Windows.Forms.Label lblTypeAgence;
     }
 }
