570ad12f1775b6b9a9dee063791f52e3dd789c1d
Cartagene <45066097+Cartagene@users.noreply.github.com>
2019-10-10 09:58:49 +0200

    Evenementiel

---
+++ GesCampagneBO/Evenementiel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GesCampagneBO
+{
+    public class Evenementiel
+    {
+        private string nom;
+        private string rue;
+        private string telephone;
+        private string mail;
+        private string site;
+        private Ville laVille; 
+    }
+}
