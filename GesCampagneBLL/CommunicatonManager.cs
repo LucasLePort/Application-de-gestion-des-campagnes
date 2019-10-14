249c05bee452b07283ab16411e265cb98178c5be
Cartagene <45066097+Cartagene@users.noreply.github.com>
2019-10-14 14:39:41 +0200

    Update CommunicatonManager.cs

--- GesCampagneBLL/CommunicatonManager.cs
+++ GesCampagneBLL/CommunicatonManager.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using GesCampagneBO;
+using GesCampagneDAL;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,65 @@ namespace GesCampagneBLL
 {
     public class CommunicatonManager
     {
+        private static CommunicatonManager uneInstance;
+
+        //cette méthode crée un objet de la classe CommunicatonManager s'il n'existe pas déja un
+        //puis retourne la référence à cet objet
+        public static CommunicatonManager GetInstance()
+        {
+            if (uneInstance == null)
+            {
+                uneInstance = new CommunicatonManager();
+            }
+            return uneInstance;
+        }
+        //le constructeur par défaut est privé: il ne sera pas possible de créer un
+        //objet à l'extérieur de la classe avec l'instruction new...
+        private CommunicatonManager()
+        {
+
+        }
+
+        //appel de la couche DAL pour récupérer une collection de communications
+        public List<Communication> GetCommunications()
+        {
+            //ici on peut appliquer des règles métier
+            return CommunicationDAO.GetInstance().GetCommunications();
+        }
+
+        //appel de la couche DAL ppur créer une nouvelle agence de Communication 
+        public int CreerCommunication(string sonNom, string saRue, string sonTelephone, string sonMail, string sonSite, int saVille, out string erreur)
+        {
+            erreur = "";
+            Communication laComm;
+            Ville laVille;
+            int ajoutComm = 0;
+
+            if(sonNom=="")
+            {
+                erreur += "\nVeuillez saisir le nom du l'agence";
+            }
+            if (saRue == "")
+            {
+                erreur += "\nVeuillez saisir la rue de l'agence";
+            }
+            if (sonTelephone == "")
+            {
+                erreur += "\nVeuillez saisir le numéro de téléphone de l'agence";
+            }
+            if (sonMail == "")
+            {
+                erreur += "\nVeuillez saisir l'adresse mail de l'agence";
+            }
+            if (sonNom == "")
+            {
+                erreur += "\nVeuillez saisir le nom du l'agence";
+            }
+            if (sonNom == "")
+            {
+                erreur += "\nVeuillez saisir le nom du l'agence";
+            }
+
+        }
     }
 }
