00f3f1bfca62fd27a3516a457590937d71b535ac
Pierre Landry <45065999+Linkale@users.noreply.github.com>
2019-10-15 08:23:53 +0200

    salut

--- GesCampagneBLL/EvenementManager.cs
+++ GesCampagneBLL/EvenementManager.cs
@@ -51,7 +51,6 @@ namespace GesCampagneBLL
             {
                 msgErreur += "\nVeuillez saisir le libellé de l'événement";
             }
-            if (saDateDebut == null )
             if (saDateDebut == null)
             {
                 msgErreur += "\nVeuillez saisir la date du début de l'événement";
