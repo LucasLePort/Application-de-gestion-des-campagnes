61dbf8a3db671c1dcc310d8ef6e5f255f2c4c445
Pierre Landry <45065999+Linkale@users.noreply.github.com>
2019-11-04 16:43:54 +0100

    rt

--- GesCampagneGUI/FrmConsultEvenement.Designer.cs
+++ GesCampagneGUI/FrmConsultEvenement.Designer.cs
@@ -29,26 +29,123 @@
         private void InitializeComponent()
         {
             this.dtgConsultEvenement = new System.Windows.Forms.DataGridView();
+            this.pnlPeriode = new System.Windows.Forms.Panel();
+            this.lblPeriode = new System.Windows.Forms.Label();
+            this.pnlChoixVille = new System.Windows.Forms.Panel();
+            this.lblChoixVille = new System.Windows.Forms.Label();
+            this.cbxChoixVille = new System.Windows.Forms.ComboBox();
+            this.dtpDebut = new System.Windows.Forms.DateTimePicker();
+            this.dtpFin = new System.Windows.Forms.DateTimePicker();
+            this.lblPeriodeDebut = new System.Windows.Forms.Label();
+            this.lblPeriodeFin = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dtgConsultEvenement)).BeginInit();
+            this.pnlPeriode.SuspendLayout();
+            this.pnlChoixVille.SuspendLayout();
             this.SuspendLayout();
             // 
             // dtgConsultEvenement
             // 
             this.dtgConsultEvenement.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dtgConsultEvenement.Location = new System.Drawing.Point(70, 29);
+            this.dtgConsultEvenement.Location = new System.Drawing.Point(70, 161);
             this.dtgConsultEvenement.Name = "dtgConsultEvenement";
-            this.dtgConsultEvenement.Size = new System.Drawing.Size(648, 345);
+            this.dtgConsultEvenement.Size = new System.Drawing.Size(657, 213);
             this.dtgConsultEvenement.TabIndex = 0;
+            // 
+            // pnlPeriode
+            // 
+            this.pnlPeriode.Controls.Add(this.lblPeriodeFin);
+            this.pnlPeriode.Controls.Add(this.lblPeriodeDebut);
+            this.pnlPeriode.Controls.Add(this.dtpFin);
+            this.pnlPeriode.Controls.Add(this.dtpDebut);
+            this.pnlPeriode.Controls.Add(this.lblPeriode);
+            this.pnlPeriode.Location = new System.Drawing.Point(70, 4);
+            this.pnlPeriode.Name = "pnlPeriode";
+            this.pnlPeriode.Size = new System.Drawing.Size(374, 82);
+            this.pnlPeriode.TabIndex = 1;
+            // 
+            // lblPeriode
+            // 
+            this.lblPeriode.AutoSize = true;
+            this.lblPeriode.Location = new System.Drawing.Point(3, 5);
+            this.lblPeriode.Name = "lblPeriode";
+            this.lblPeriode.Size = new System.Drawing.Size(76, 13);
+            this.lblPeriode.TabIndex = 0;
+            this.lblPeriode.Text = "Saisie période:";
+            // 
+            // pnlChoixVille
+            // 
+            this.pnlChoixVille.Controls.Add(this.cbxChoixVille);
+            this.pnlChoixVille.Controls.Add(this.lblChoixVille);
+            this.pnlChoixVille.Location = new System.Drawing.Point(70, 104);
+            this.pnlChoixVille.Name = "pnlChoixVille";
+            this.pnlChoixVille.Size = new System.Drawing.Size(374, 51);
+            this.pnlChoixVille.TabIndex = 2;
+            // 
+            // lblChoixVille
+            // 
+            this.lblChoixVille.AutoSize = true;
+            this.lblChoixVille.Location = new System.Drawing.Point(13, 16);
+            this.lblChoixVille.Name = "lblChoixVille";
+            this.lblChoixVille.Size = new System.Drawing.Size(58, 13);
+            this.lblChoixVille.TabIndex = 0;
+            this.lblChoixVille.Text = "Choix Ville:";
+            // 
+            // cbxChoixVille
+            // 
+            this.cbxChoixVille.FormattingEnabled = true;
+            this.cbxChoixVille.Location = new System.Drawing.Point(100, 16);
+            this.cbxChoixVille.Name = "cbxChoixVille";
+            this.cbxChoixVille.Size = new System.Drawing.Size(179, 21);
+            this.cbxChoixVille.TabIndex = 1;
+            // 
+            // dtpDebut
+            // 
+            this.dtpDebut.Location = new System.Drawing.Point(145, 16);
+            this.dtpDebut.Name = "dtpDebut";
+            this.dtpDebut.Size = new System.Drawing.Size(200, 20);
+            this.dtpDebut.TabIndex = 1;
+            this.dtpDebut.ValueChanged += new System.EventHandler(this.dtpDebut_ValueChanged);
+            // 
+            // dtpFin
+            // 
+            this.dtpFin.Location = new System.Drawing.Point(145, 53);
+            this.dtpFin.Name = "dtpFin";
+            this.dtpFin.Size = new System.Drawing.Size(200, 20);
+            this.dtpFin.TabIndex = 2;
+            // 
+            // lblPeriodeDebut
+            // 
+            this.lblPeriodeDebut.AutoSize = true;
+            this.lblPeriodeDebut.Location = new System.Drawing.Point(37, 22);
+            this.lblPeriodeDebut.Name = "lblPeriodeDebut";
+            this.lblPeriodeDebut.Size = new System.Drawing.Size(63, 13);
+            this.lblPeriodeDebut.TabIndex = 3;
+            this.lblPeriodeDebut.Text = "Date début:";
+            // 
+            // lblPeriodeFin
+            // 
+            this.lblPeriodeFin.AutoSize = true;
+            this.lblPeriodeFin.Location = new System.Drawing.Point(40, 59);
+            this.lblPeriodeFin.Name = "lblPeriodeFin";
+            this.lblPeriodeFin.Size = new System.Drawing.Size(47, 13);
+            this.lblPeriodeFin.TabIndex = 4;
+            this.lblPeriodeFin.Text = "Date fin:";
             // 
             // FrmConsultEvenement
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pnlChoixVille);
+            this.Controls.Add(this.pnlPeriode);
             this.Controls.Add(this.dtgConsultEvenement);
             this.Name = "FrmConsultEvenement";
             this.Text = "FrmConsultEvenement";
             ((System.ComponentModel.ISupportInitialize)(this.dtgConsultEvenement)).EndInit();
+            this.pnlPeriode.ResumeLayout(false);
+            this.pnlPeriode.PerformLayout();
+            this.pnlChoixVille.ResumeLayout(false);
+            this.pnlChoixVille.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -56,5 +153,14 @@
         #endregion
 
         private System.Windows.Forms.DataGridView dtgConsultEvenement;
+        private System.Windows.Forms.Panel pnlPeriode;
+        private System.Windows.Forms.Label lblPeriode;
+        private System.Windows.Forms.Panel pnlChoixVille;
+        private System.Windows.Forms.Label lblChoixVille;
+        private System.Windows.Forms.ComboBox cbxChoixVille;
+        private System.Windows.Forms.DateTimePicker dtpFin;
+        private System.Windows.Forms.DateTimePicker dtpDebut;
+        private System.Windows.Forms.Label lblPeriodeFin;
+        private System.Windows.Forms.Label lblPeriodeDebut;
     }
 }
